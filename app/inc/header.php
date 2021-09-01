5cd64277a207d36d6d2a4c197dc00a7f3549aa8b
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-09-01 10:54:49 -0500

    create template header

---
+++ app/inc/header.php
@@ -0,0 +1,29 @@
+<?PHP include("./db.php"); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aca 1</title>
+    <link href="./assets/css/style.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+  <header>
+    <nav class="navbar navbar-ligth bg-ligth">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+          </svg>
+          Contactos ACA 1
+        </a>
+        <button type="button" class="btn btn-primary btn-contact">
+          <i class="bi bi-person-plus"></i>
+          Agregar Contacto
+        </button>
+      </div>
+    </nav>
+  </header>
